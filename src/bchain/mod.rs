52693faa7edf9fc7a6e7213cc628063d359e430e
Wisdom Praise <wisdompraise968@gmail.com>
2021-06-26 13:42:35 +0100

    Initialized  the blockchain

--- src/bchain/mod.rs
+++ src/bchain/mod.rs
@@ -1,9 +1,9 @@
 extern crate blake2;
 
-use std::collections::HashMap;
-use std::time::SystemTime;
 use blake2::{Blake2b, Digest};
-
+use std::collections::HashMap;
+use std::result::Result;
+use std::time::SystemTime;
 
 //blockchain container
 #[derive(Debug, Clone)]
@@ -13,10 +13,12 @@ pub struct Blockchain {
     pending_transactions: Vec<Transaction>,
 }
 
-trait WorldState {
+pub trait WorldState {
     fn get_user_ids(&self) -> Vec<String>;
-    fn get_account_by_id(&mut self, id: &String) -> Option<&mut Account>;
-    fn create_account(&mut self, id: String, account_type: AccountType) -> Result<(), &'static str>;
+    fn get_account_by_id_mut(&mut self, id: &String) -> Option<&mut Account>;
+    fn get_account_by_id(&self, id: &String) -> Option<&Account>;
+    fn create_account(&mut self, id: String, account_type: AccountType)
+        -> Result<(), &'static str>;
 }
 
 #[derive(Debug, Clone)]
@@ -39,8 +41,9 @@ pub struct Transaction {
 #[derive(Debug, Clone, PartialEq)]
 pub enum TransactionData {
     CreateUserAccount(String),
-    ChangeStoreValue {key: String, value: String},
-    TransferTokens {to: String, amount: u128}
+    ChangeStoreValue { key: String, value: String },
+    TransferTokens { to: String, amount: u128 },
+    CreateTokens { receiver: String, amount: u128 },
 }
 
 #[derive(Debug, Clone)]
@@ -57,12 +60,12 @@ pub enum AccountType {
     Validator {
         correctly_validated_blocks: u128,
         incorrectly_validated_blocks: u128,
-        you_get_the_idea: bool
-    }
+        you_get_the_idea: bool,
+    },
 }
 
 impl Account {
-    pub fn new(account_type, AccountType) -> Self {
+    pub fn new(account_type: AccountType) -> Self {
         return Self {
             tokens: 0,
             acc_type: account_type,
@@ -88,49 +91,55 @@ impl Blockchain {
         let is_genesis = self.len() == 0;
 
         if !block.verify_own_hash() {
-            Err("The block hash is mismatching! (Code: 93820394)".into())
+            return Err("The block hash is mismatching! (Code: 93820394)".into());
         }
-        
         if !(block.prev_hash == self.get_last_block_hash()) {
-            Err("The new block has to point to the previous block (Code: 3948230)".into())
+            return Err("The new block has to point to the previous block (Code: 3948230)".into());
         }
-        
         if block.get_transaction_count() == 0 {
-            Err("There has to be at least one transaction inside the block! (Code: 9482930)".into())
+            return Err(
+                "There has to be at least one transaction inside the block! (Code: 9482930)".into(),
+            );
         }
 
         let old_state = self.accounts.clone();
         for (i, transaction) in block.transactions.iter().enumerate() {
             if let Err(err) = transaction.execute(self, &is_genesis) {
                 self.accounts = old_state;
-                Err(format!("Could not execute transaction {} due to `{}`. Rolling back (Code: 38203984)", i + 1, err))
+                return Err(format!(
+                    "Could not execute transaction {} due to `{}`. Rolling back (Code: 38203984)",
+                    i + 1,
+                    err
+                ));
             }
         }
+        self.blocks.push(block);
         Ok(())
     }
 
     pub fn get_last_block_hash(&self) -> Option<String> {
         if self.len() == 0 {
-            None
+            return None;
         }
         self.blocks[self.blocks.len() - 1].hash.clone()
     }
 
     pub fn check_validity(&self) -> Result<(), String> {
         for (block_num, block) in self.blocks.iter().enumerate() {
-
             // Check if block saved hash matches to calculated hash
             if !block.verify_own_hash() {
-                return Err(format!("Stored hash for Block #{} \
-                    does not match calculated hash (Code: 665234234)", block_num + 1).into());
+                return Err(format!(
+                    "Stored hash for Block #{} does not match calculated hash (Code: 665234234)",
+                    block_num + 1
+                )
+                .into());
             }
 
             // Check previous black hash points to actual previous block
             if block_num == 0 {
                 // Genesis block should point to nowhere
                 if block.prev_hash.is_some() {
-                    return Err("The genesis block has a previous hash set which \
-                     it shouldn't Code :394823098".into());
+                    return Err("The genesis block has a previous hash set which it shouldn't Code :394823098".into());
                 }
             } else {
                 // Non genesis blocks should point to previous blocks hash (which is validated before)
@@ -143,20 +152,20 @@ impl Blockchain {
                 let prev_hash_actual = self.blocks[block_num - 1].hash.as_ref().unwrap();
 
                 if !(&block.prev_hash == &self.blocks[block_num - 1].hash) {
-                    return Err(format!("Block #{} is not connected to previous block (Hashes do \
-                    not match. Should be `{}` but is `{}`)", block_num, prev_hash_proposed,
-                                       prev_hash_actual).into());
+                    return Err(format!("Block #{} is not connected to previous block (Hashes do not match. Should be `{}` but is `{}`)", block_num, prev_hash_proposed, prev_hash_actual).into());
                 }
             }
 
             // Check if transactions are signed correctly
             for (transaction_num, transaction) in block.transactions.iter().enumerate() {
-
                 // Careful! With that implementation an unsigned message will always
                 // be valid! You may remove the first check to only accept signed transactions
                 if transaction.is_signed() && !transaction.check_signature() {
-                    return Err(format!("Transaction #{} for Block #{} has an invalid signature \
-                    (Code: 4398239048)", transaction_num + 1, block_num + 1));
+                    return Err(format!(
+                        "Transaction #{} for Block #{} has an invalid signature (Code: 4398239048)",
+                        transaction_num + 1,
+                        block_num + 1
+                    ));
                 }
             }
         }
@@ -178,15 +187,15 @@ impl Block {
         let mut hasher = Blake2b::new();
 
         for transaction in self.transactions.iter() {
-            hasher.update(transaction.calculate_hash)
+            hasher.update(transaction.calculate_hash())
         }
 
-        let block_as_string = format!("{:?}", (&self.prev_hash, &self.nonce))
-        hasher.update(&block_as_string)
+        let block_as_string = format!("{:?}", (&self.prev_hash, &self.nonce));
+        hasher.update(&block_as_string);
         Vec::from(hasher.finalize().as_ref())
     }
 
-        /// Appends a transaction to the queue
+    /// Appends a transaction to the queue
     pub fn add_transaction(&mut self, transaction: Transaction) {
         self.transactions.push(transaction);
         self.update_hash();
@@ -204,9 +213,13 @@ impl Block {
     }
 
     pub fn verify_own_hash(&self) -> bool {
-        if self.hash.is_some() && self.hash.as_ref().unwrap().eq(
-            &byte_vector_to_string(&self.calculate_hash())
-        ) {
+        if self.hash.is_some()
+            && self
+                .hash
+                .as_ref()
+                .unwrap()
+                .eq(&byte_vector_to_string(&self.calculate_hash()))
+        {
             return true;
         }
         false
@@ -226,7 +239,11 @@ impl WorldState for Blockchain {
         self.accounts.get(id)
     }
 
-    fn create_account(&mut self, id: String, account_type: AccountType) -> Result<(), &'static str> {
+    fn create_account(
+        &mut self,
+        id: String,
+        account_type: AccountType,
+    ) -> Result<(), &'static str> {
         return if !self.get_user_ids().contains(&id) {
             let acc = Account::new(account_type);
             self.accounts.insert(id, acc);
@@ -236,7 +253,6 @@ impl WorldState for Blockchain {
         };
     }
 }
-
 
 impl Transaction {
     pub fn new(from: String, transaction_data: TransactionData, nonce: u128) -> Self {
@@ -249,8 +265,12 @@ impl Transaction {
         }
     }
 
-    pub fn execute<T: WorldState>(&self, world_state: &mut T, is_initial: &bool) -> Result<(), &'static str> {
-         // Check if sending user does exist (no one not on the chain can execute transactions)
+    pub fn execute<T: WorldState>(
+        &self,
+        world_state: &mut T,
+        is_initial: &bool,
+    ) -> Result<(), &'static str> {
+        // Check if sending user does exist (no one not on the chain can execute transactions)
         if let Some(_account) = world_state.get_account_by_id(&self.from) {
             // Do some more checkups later on...
         } else {
@@ -262,14 +282,15 @@ impl Transaction {
         // match is like a switch (pattern matching) in C++ or Java
         // We will check for the type of transaction here and execute its logic
         return match &self.record {
-
             TransactionData::CreateUserAccount(account) => {
                 world_state.create_account(account.into(), AccountType::User)
             }
 
             TransactionData::CreateTokens { receiver, amount } => {
                 if !is_initial {
-                    return Err("Token creation is only available on initial creation (Code: 2394233)");
+                    return Err(
+                        "Token creation is only available on initial creation (Code: 2394233)",
+                    );
                 }
                 // Get the receiving user (must exist)
                 return if let Some(account) = world_state.get_account_by_id_mut(receiver) {
@@ -301,15 +322,20 @@ impl Transaction {
                 let balance_sender_new = sender_tokens.checked_sub(*amount);
 
                 if balance_recv_new.is_some() && balance_sender_new.is_some() {
-                    world_state.get_account_by_id_mut(&self.from).unwrap().tokens = balance_sender_new.unwrap();
-                    world_state.get_account_by_id_mut(to).unwrap().tokens = balance_recv_new.unwrap();
+                    world_state
+                        .get_account_by_id_mut(&self.from)
+                        .unwrap()
+                        .tokens = balance_sender_new.unwrap();
+                    world_state.get_account_by_id_mut(to).unwrap().tokens =
+                        balance_recv_new.unwrap();
                     return Ok(());
                 } else {
                     return Err("Overspent or Arithmetic error (Code: 48239084203)");
                 }
             }
 
-            _ => { // Not implemented transaction type
+            _ => {
+                // Not implemented transaction type
                 Err("Unknown Transaction type (not implemented) (Code: 487289724389)")
             }
         };
@@ -318,7 +344,10 @@ impl Transaction {
     /// Will calculate the hash using Blake2 hasher
     pub fn calculate_hash(&self) -> Vec<u8> {
         let mut hasher = Blake2b::new();
-        let transaction_as_string = format!("{:?}", (&self.created_at, &self.record, &self.from, &self.nonce));
+        let transaction_as_string = format!(
+            "{:?}",
+            (&self.created_at, &self.record, &self.from, &self.nonce)
+        );
 
         hasher.update(&transaction_as_string);
         return Vec::from(hasher.finalize().as_ref());
@@ -340,7 +369,6 @@ impl Transaction {
         self.signature.is_some()
     }
 }
-
 
 /// Will take an array of bytes and transform it into a string by interpreting every byte
 /// as an character due to RFC 1023 that's not possible
